ce0956c9403e3357dfad1e10cd6e04ab36b166c7
smghacker <valeri.colov@gmail.com>
2025-07-30 13:10:01 +0300

    Simple admin for tree view without similarities

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,68 @@
+import nested_admin
+from django.template.response import TemplateResponse
+from django.urls import path
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
-# Register your models here.
+from .models import Category, SimilarCategory
+
+
+class SimilarCategoryInline(nested_admin.NestedTabularInline):
+    model = SimilarCategory
+    fk_name = "category_a"
+    extra = 1
+    autocomplete_fields = ["category_b"]
+    verbose_name = "Similar to"
+    verbose_name_plural = "Similar categories"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.resolver_match.view_name.endswith("change"):
+            return qs  # full queryset for admin filters to work
+        return qs.none()  # limit to top 20 for performance
+
+
+class ChildCategoryInline(nested_admin.NestedTabularInline):
+    model = Category
+    extra = 1
+    fk_name = 'parent'
+    inlines = [SimilarCategoryInline]
+    show_change_link = True
+    autocomplete_fields = ["parent"]
+
+
+def view_similar_links(obj):
+    url = reverse("admin:catalog_similarcategory_changelist") + f"?category_a__id__exact={obj.id}"
+    return format_html("<a href='{}' target='_blank'>View all</a>", url)
+
+view_similar_links.short_description = "All Similar"
+
+
+@admin.register(Category)
+class CategoryAdmin(nested_admin.NestedModelAdmin):
+    list_display = ["id", "name", "parent", view_similar_links]
+    search_fields = ["name", "description"]
+    inlines = [ChildCategoryInline, SimilarCategoryInline]
+    autocomplete_fields = ["parent"]
+
+    def get_urls(self):
+        default_urls = super().get_urls()
+        custom_urls = [
+            path(
+                "tree-view/",
+                self.admin_site.admin_view(self.category_tree_view),
+                name="category_tree_view",
+            )
+        ]
+        return custom_urls + default_urls
+
+    def category_tree_view(self, request):
+        return TemplateResponse(request, "admin/category_tree_admin.html", {})
+
+
+@admin.register(SimilarCategory)
+class SimilarCategoryAdmin(admin.ModelAdmin):
+    list_display = ["category_a", "category_b"]
+    autocomplete_fields = ["category_a", "category_b"]
+    search_fields = ["category_a__name", "category_b__name"]
--- ebag_backend/urls.py
+++ ebag_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('nested_admin/', include('nested_admin.urls')),
     path("admin/", admin.site.urls),
     path("api/", include("catalog.urls")),
 ]
